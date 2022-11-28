69dba450fa37540c550da5bf14bb429a967c7c38
Igor Moura <igor.moura254@hotmail.com>
2022-11-28 18:53:08 -0300

    Creating some models and overriding OnConfiguring

---
+++ EntityFrameworkCourse/EntityFrameworkCourse/Data/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using EntityFrameworkCourse.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCourse.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Department>? Departments { get; set; }
+        public DbSet<Employee>? Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Trazer de alguma configuração externa, NÃO deixar hardcode
+            const string connectionString = "Server=localhost; Database=EntityFramework; User Id=sa; Password=Cerc@tr0va-sqlserver;";
+
+            optionsBuilder
+                // Estaremos utilizando Sql Server na seguinte connection String
+                .UseSqlServer(connectionString);
+        }
+    }
+}
